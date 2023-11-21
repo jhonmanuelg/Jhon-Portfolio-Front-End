790ad53f7230984a4e9c5ed448f466e81a656175
Jhon Manuel <jhonmanuelg6@outlook.com>
2023-11-21 03:47:34 -0300

    Uploading: Javascript file is created for event to open project page and social networks v2

---
+++ eventoMenu/menu.js
@@ -0,0 +1,16 @@
+// función menu responsibo
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+
---
+++ eventoMenu/openLink.js
@@ -0,0 +1,42 @@
+// función de WhatsApp
+function contactarPorWhatsApp() {
+    // Número de teléfono en el formato internacional
+    var numero = "5511958838782"; 
+  
+    // Crear el enlace de WhatsApp
+    var enlaceWhatsApp = "https://wa.me/" + numero;
+  
+    // Abrir enlace en una nueva ventana o pestaña
+    window.open(enlaceWhatsApp, "_blank");
+  }
+
+
+
+// link para abrir las paginas en otra ventana redes sociales
+const githubURL = "https://github.com/jhonmanuelg";
+const twitterURL = "https://twitter.com/JhonManuelGil2";
+const linkedinURL = "https://www.linkedin.com/in/jhon-manuel-gil/";
+
+// evento click redes sociales
+function openLink(event, link) {
+  event.preventDefault();
+  // Abrir enlace en una nueva ventana 
+  window.open(link, "_blank");
+}
+
+// link para abrir los proyectos en otra ventana
+const netflixUrl = "https://jhonmanuelg.github.io/Netflix-Landing-Page-Website/";
+const netflixGit = "https://github.com/jhonmanuelg/Netflix-Landing-Page-Website";
+
+const crudUrl = "https://jhonmanuelg.github.io/Operaciones_CRUD_JavaScript/"; 
+const crudGit = "https://github.com/jhonmanuelg/Operaciones_CRUD_JavaScript";
+
+const pageUrl = "https://jhonmanuelg.github.io/Landing_Page_using_Bootstrap_5/";
+const pageGit = "https://github.com/jhonmanuelg/Landing_Page_using_Bootstrap_5";
+
+// evento click proyectos
+function projectLink(event, link) {
+    event.preventDefault();
+    // Abrir enlace en una nueva ventana 
+    window.open(link, "_blank");
+} 
