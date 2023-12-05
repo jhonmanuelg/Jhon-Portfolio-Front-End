946e109f7b9e0dd1c7ae7ed6040ae91f6cb90489
Jhon Manuel Gil <jhonmanuelg6@gmail.com>
2023-12-05 04:04:13 -0300

    Uploading: adjusted a, box_span

--- eventoMenu/openLink.js
+++ eventoMenu/openLink.js
@@ -19,14 +19,6 @@ const twitterURL = "https://twitter.com/JhonManuelGil2";
 const  discordURL = "https://discord.com/users/835269507663659048";
 const linkedinURL = "https://www.linkedin.com/in/jhon-manuel-gil/";
 
-
-// evento click redes sociales
-function openLink(event, link) {
-  event.preventDefault();
-  // Abrir enlace en una nueva ventana 
-  window.open(link, "_blank");
-}
-
 // link para abrir los proyectos en otra ventana
 const netflixUrl = "https://jhonmanuelg.github.io/Netflix-Landing-Page-Website/";
 const netflixGit = "https://github.com/jhonmanuelg/Netflix-Landing-Page-Website";
@@ -37,9 +29,11 @@ const crudGit = "https://github.com/jhonmanuelg/Operaciones_CRUD_JavaScript";
 const pageUrl = "https://jhonmanuelg.github.io/Landing_Page_using_Bootstrap_5/";
 const pageGit = "https://github.com/jhonmanuelg/Landing_Page_using_Bootstrap_5";
 
-// evento click proyectos
-function projectLink(event, link) {
-    event.preventDefault();
-    // Abrir enlace en una nueva ventana 
-    window.open(link, "_blank");
-} 
+
+// evento click redes sociales
+function openLink(event, link) {
+  event.preventDefault();
+  // Abrir enlace en una nueva ventana 
+  window.open(link, "_blank");
+}
+
