8b5bbdc3b341c357853b2c22284fc44ee5ecdaec
Jhon Manuel Gil <jhonmanuelg6@gmail.com>
2023-12-30 06:14:32 -0300

    Uploading: WhatsApp function is updated README.md V.2

--- eventoMenu/openLink.js
+++ eventoMenu/openLink.js
@@ -5,33 +5,33 @@ var whatsappButtons = document.getElementsByClassName('whatsappButton');
 
 //  clase "whatsappButton"
 for (var i = 0; i < whatsappButtons.length; i++) {
-    // Asignar un controlador de eventos botón
-    whatsappButtons[i].addEventListener('click', function() {
-        // número de teléfono al que deseas enviar el mensaje
-        var phoneNumber = '5511958838782';
+  // Asignar un controlador de eventos botón
+  whatsappButtons[i].addEventListener('click', function () {
+    // número de teléfono al que deseas enviar el mensaje
+    var phoneNumber = '5511958838782';
 
-        // el mensaje predeterminado
-        var message = 'Olá, sou Jhon MAnuel Gil muito obrigado por entrar em contato comigo.';
+    // el mensaje predeterminado
+    var message = 'Olá, sou Jhon MAnuel Gil muito obrigado por entrar em contato comigo.';
 
-        // Construir la URL de WhatsApp
-        var whatsappURL = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + encodeURIComponent(message);
+    // Construir la URL de WhatsApp
+    var whatsappURL = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + encodeURIComponent(message);
 
-        // Abrir WhatsApp en una nueva ventana o pestaña
-        window.open(whatsappURL, '_blank');
-    });
+    // Abrir WhatsApp en una nueva ventana o pestaña
+    window.open(whatsappURL, '_blank');
+  });
 }
 
 // link para abrir las paginas en otra ventana redes sociales
 const githubURL = "https://github.com/jhonmanuelg";
 const twitterURL = "https://twitter.com/JhonManuelGil2";
-const  discordURL = "https://discord.com/users/835269507663659048";
+const discordURL = "https://discord.com/users/835269507663659048";
 const linkedinURL = "https://www.linkedin.com/in/jhon-manuel-gil/";
 
 // link para abrir los proyectos en otra ventana
 const netflixUrl = "https://jhonmanuelg.github.io/Netflix-Landing-Page-Website/";
 const netflixGit = "https://github.com/jhonmanuelg/Netflix-Landing-Page-Website";
 
-const crudUrl = "https://jhonmanuelg.github.io/Operaciones_CRUD_JavaScript/"; 
+const crudUrl = "https://jhonmanuelg.github.io/Operaciones_CRUD_JavaScript/";
 const crudGit = "https://github.com/jhonmanuelg/Operaciones_CRUD_JavaScript";
 
 const pageUrl = "https://jhonmanuelg.github.io/Landing_Page_using_Bootstrap_5/";
